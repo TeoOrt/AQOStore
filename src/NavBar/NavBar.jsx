a162f89d0456fd3c4e8857f6c2eff3bc6cb063b0
Mateo Ortega <mateo041ortega@gmail.com>
2023-02-23 13:16:31 -0700

    Fished the navbar for mobile devices

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,35 +1,33 @@
 import React from "react";
-import AqoLogo from "./AQOLOGO.svg";
+import AqoLogo from "./AQOLOGOSVG.svg";
 import UserBox from "./User_box_fill.svg";
 import UserCart from "./Bag_alt_fill.svg";
-import styles from "./NavBarStyles";
+import "./NavBar.css";
+
 function Navbar() {
   return (
-    <nav style={styles.nav}>
-      <div className="left-section" style={styles.section}>
-        <img src={AqoLogo} alt="Logo" style={styles.AqoLogo} />
-        <h1 style={styles.h1}>AQO Store</h1>
+    <nav className="nav">
+      <div className="section">
+        <img src={AqoLogo} alt="Logo" className="navbar-logo" />
+        <h1 className="navbar-title">AQO Store</h1>
       </div>
-      <div className="middle-section" style={styles.section}>
-        <a href="https://www.instagram.com/aqo.jewelry/" style={styles.FindUs}>
-          Find us
+      <div className="section">
+        <a
+          href="https://www.instagram.com/aqo.jewelry/"
+          className="navbar-link"
+        >
+          Jewelry
         </a>
-        <a href="#" style={styles.a}>
-          Shop
-        </a>
-        <a href="#" style={styles.a}>
-          FAQ
-        </a>
-        <a href="#" style={styles.a}>
-          Our Story
+        <a href="/" className="navbar-link">
+          Accesories
         </a>
       </div>
-      <div className="right-section" style={styles.section}>
-        <a href="#">
-          <img src={UserCart} alt="Cart" />
+      <div className="section">
+        <a href="/">
+          <img src={UserCart} alt="Cart" className="navbar-icon" />
         </a>
         <a href="/Login">
-          <img src={UserBox} alt="Account" />
+          <img src={UserBox} alt="Account" className="navbar-icon" />
         </a>
       </div>
     </nav>
